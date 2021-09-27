aa9b7555053bd699fae6d5ff11a312c7b6b89bcd
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-27 15:33:13 +0100

    Balance is increased by top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,19 @@
 require './lib/oystercard'
 
 describe Oystercard do
-  it { is_expected.to respond_to(:top_up).with(1).argument }
-
+  
   it 'gives a new oystercard with a balance of 0' do
     expect(subject.balance).to eq 0
   end
-
+  
   describe '#top_up' do
-    # it 'tops up oystercard by £10' do
-    #   subject.top_up(10)
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    
+    it 'tops up oystercard by £10' do
+      subject.top_up(10)
 
-    #   expect(subject.balance).to eq 10
-    # end
+      expect(subject.balance).to eq 10
+    end
+  
   end
 end
